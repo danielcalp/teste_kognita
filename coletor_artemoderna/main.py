e907a566091e3622796a9a06d683432565fe45b6
Daniel <daniel.paschoalino@aluno.faculdadeimpacta.com.br>
2023-04-04 13:30:38 -0300

    Primeiro Commit

--- coletor_artemoderna/main.py
+++ coletor_artemoderna/main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-from coletor_artemoderna.source.artemoderna import ArteModerna
+from source.artemoderna import ArteModerna
 
 if __name__ == '__main__':
     all_items_to_save = ArteModerna().start_requests()
-    df = pd.DataFrame(all_items_to_save)
-    df.to_csv('results.csv')
+    print(all_items_to_save)
+    # df = pd.DataFrame(all_items_to_save)
+    # df.to_csv('results.csv')
 
--- coletor_artemoderna/source/artemoderna.py
+++ coletor_artemoderna/source/artemoderna.py
@@ -1,23 +1,54 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import requests
+import json
+from bs4 import BeautifulSoup
 
 
-class ArteModerna:
+def create_dict_extracted_data():
+    dict_extracted_data = {
+        'ID' : '',
+        'url': '',
+        'artistas': '',
+        'titulo': '',
+        'preco': '',
+        'imagem': '',
+        'composicao': '',
+        'descricao': '',
+        'categorias': ''
+    }
+    return dict_extracted_data
+
+# def 
+
+
+class ArteModerna():
     """
     Main Class
     """
-    parameters_to_crawl = ['http://pstrial-2019-12-16.toscrape.com/browse/qingjapanese',
-                           'http://pstrial-2019-12-16.toscrape.com/browse/unetete']
+    # with open("C:\\Users\\Daniel\\Desktop\\teste_kognita\\kognita-da-teste\\Categorias_subcategorias.json", "w", encoding='utf-8') as outfile:
+    #     json.dump(dict_identificação_imagens_pacientes, outfile, ensure_ascii=False)
 
     def start_requests(self):
         print('logic here')
-        all_parsed_items = []
+        #####
+        all_url_categories = []
+        dict_extracted_data = create_dict_extracted_data()
         ###
-        ###
-        item_to_parse = 'http://pstrial-2019-12-16.toscrape.com/item/14879/Jacques_Tuboeuf'
-        item_parsed = self.parse_items(value_to_parse=item_to_parse)
-        all_parsed_items.append(item_parsed)
-        return all_parsed_items
+        url_base = 'http://pstrial-2019-12-16.toscrape.com/browse/'
+        s = requests.Session()
+        response = s.get(url_base)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        categorias = soup.find('div', {'id': 'subcats'})
+        lista_categorias = categorias.find_all('div')
+        return lista_categorias
+        #####
+        # item_to_parse = 'http://pstrial-2019-12-16.toscrape.com/item/14879/Jacques_Tuboeuf'
+        # item_parsed = self.parse_items(value_to_parse=item_to_parse)
+        # all_parsed_items.append(item_parsed)
+        # return all_parsed_items
+    
 
     def parse_items(self, value_to_parse: str):
         print('parse individual arts here')
